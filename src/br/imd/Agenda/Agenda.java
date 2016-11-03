d33d8f975567f446ddc19eb95aaf6c9a117d854d
João Victor <victorbarboza@outlook.com>
2016-11-03 02:03:19 +0000

    novas modificações

--- src/br/imd/Agenda/Agenda.java
+++ src/br/imd/Agenda/Agenda.java
@@ -4,13 +4,18 @@ import br.imd.Arvore.TADArvore;
 
 public class Agenda {
 	private TADArvore lista;
-
-	public void armazenaPessoa(Pessoa pessoa) {
-
+	
+	public Agenda(){
+	    this.lista = new TADArvore();
 	}
 
-	public void removePessoa(String Nome) {
+	public void armazenaPessoa(Pessoa pessoa) {
+        this.lista.inserir(new Node(Pessoa));
+	}
 
+	public void removePessoa(String nome) {
+        Pessoa p = lista.buscarProfundidadeObj();
+        lista.remover();//tem que enviar um nó
 	}
 
 	public int buscarPessoa(String nome) {
